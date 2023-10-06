dc56d34a3e26abe7c04b2b2600916d20c8bb106d
nguyenquang <nguyenquangos0302git@gmail.com>
2023-10-06 20:19:18 +0700

    upload

--- spring-sec/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ spring-sec/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.eazybytes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
@@ -10,19 +11,37 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 public class ProjectSecurityConfig {
 
-    /**
-     * below is the custom security configurations
-     */
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    @Order(1)
+    SecurityFilterChain defaultSecurityFilterChain1(HttpSecurity http) throws Exception {
         http
                 .cors().disable()
                 .csrf().disable()
-                .authorizeHttpRequests((requests) -> {
-                    requests.requestMatchers(AntPathRequestMatcher.antMatcher("/api/**/login")).permitAll();
-//                    requests.requestMatchers("/api/**/login").permitAll();
-                    requests.requestMatchers("/test").authenticated();
-                });
+                .securityMatcher(AntPathRequestMatcher.antMatcher("/api/**/login"))
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin();
+        http.httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain defaultSecurityFilterChain2(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .securityMatcher(AntPathRequestMatcher.antMatcher("/h2-console/**"))
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    @Order(3)
+    SecurityFilterChain defaultSecurityFilterChain3(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated());
         http.formLogin();
         http.httpBasic();
         return http.build();
